f583d80f14c63e1b9ce9d2261afc3c8cc87f9b00
fpnav <fpnav@yahoo.com.br>
2016-08-25 10:57:43 -0300

    Teste Professor

---
+++ DOMAIN/Entities/Professor.cs
@@ -0,0 +1,23 @@
+﻿using DOMAIN.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMAIN.Entities
+{
+    public class Professor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+
+        public bool IsValid()
+        {
+            return CpfService.IsValid(Cpf) &&
+                EmailService.IsValid(Email);
+        }
+    }
+}
